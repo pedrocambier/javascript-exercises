77ff35ebd4215b270003a460006d5cd750f50732
pedrocambier <pedrocambier@gmail.com>
2022-02-19 23:43:33 -0300

    first exercise complete, second in progress

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(!!string || !!num) return '';
+  if(num<0) return 'ERROR';
+  let resultString = '';
+  for (let i=0; i<num;i++) {
+    resultString += string;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
