93842fefe842b74d0c0d8572406275db7907c666
pedrocambier <pedrocambier@gmail.com>
2022-03-08 10:06:05 -0300

    exercise 08 sucessful

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return (array.reduce(((value, item) => value+item),0));
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return (array.reduce(((value, item) => value*item),1));
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a===0) return 1;
+  if (a===1) return 1;
+  return a*factorial(a-1);
 };
 
 // Do not edit below this line
