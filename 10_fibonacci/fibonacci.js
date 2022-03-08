ba8884fa418716ff1165c761233b5d8f72780775
pedrocambier <pedrocambier@gmail.com>
2022-03-08 11:56:49 -0300

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  index = parseInt(index);
+  if(index <= 0) return "OOPS";
+  if (index === 1 || index === 2) return 1;
+  return (fibonacci(index-1) + fibonacci(index-2));
 };
 
 // Do not edit below this line
