63f0fb432036e8855c99f0d85abbf49af5543ca7
pedrocambier <pedrocambier@gmail.com>
2022-03-08 12:59:23 -0300

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldest = people.reduce((prevPerson, nextPerson) => {
+    let prevPersonAge = 0;
+    let nextPersonAge = 0;
+    const thisYear = new Date().getFullYear();
+    prevPersonAge =  ("yearOfDeath" in prevPerson) ? prevPerson.yearOfDeath - prevPerson.yearOfBirth : thisYear - prevPerson.yearOfBirth;
+    nextPersonAge =  ("yearOfDeath" in nextPerson) ? nextPerson.yearOfDeath - nextPerson.yearOfBirth : thisYear - nextPerson.yearOfBirth;
+    return (prevPersonAge > nextPersonAge ? prevPerson : nextPerson);
+  });
+  return oldest;
 
 };
 
