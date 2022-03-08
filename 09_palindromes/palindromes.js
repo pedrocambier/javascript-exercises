e4dd2d4c37533f499b5070c981d493caac267c52
pedrocambier <pedrocambier@gmail.com>
2022-03-08 11:33:58 -0300

    exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+function palin (string) {
+  if(string.length === 1 || string.length === 0) return true;
+  if(string[0] !== string[string.length-1]) return false;
+  return palin(string.substring(1,string.length-1));
+}
 
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  let substring = (string.match(/[a-z]/g)).join("");
+  return (palin(substring));
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
